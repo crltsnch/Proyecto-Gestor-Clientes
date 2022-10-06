a15880971cff4893186118c56bffdfbfcd2fe64f
crltsnch <crltsnch@myuax.com>
2022-10-06 10:59:13 +0200

    Paso 2, lista de clientes

--- Gestor de Clientes/gestor/database.py
+++ Gestor de Clientes/gestor/database.py
@@ -7,4 +7,19 @@ class Cliente:
         self.nombre = nombre
         self.apellido = apellido
     
-    def __str__
+    def __str__ (self):
+        return f"({self.dni}) {self.nombre} {self.apellido}"
+
+class Clientes:
+    lista = []
+    with open(config.DATABASE_PATH, newline="\n") as fichero:
+        reader =csv.reader(fichero, delimiter=";")
+        for dni, nombre, apellido in reader:
+            cliente = Cliente(dni, nombre, apellido)
+            lista.append(cliente)
+
+
+    def buscar(dni):
+        for cliente in Clientes.lista:
+            if cliente.dni == dni:
+                return cliente
