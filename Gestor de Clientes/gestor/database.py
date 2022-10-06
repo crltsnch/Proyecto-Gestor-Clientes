46e46ed4e170c0f04035337f9f1107fba05d5298
crltsnch <crltsnch@myuax.com>
2022-10-06 11:10:23 +0200

    Paso 2 terminado

--- Gestor de Clientes/gestor/database.py
+++ Gestor de Clientes/gestor/database.py
@@ -41,4 +41,15 @@ class Clientes:
     def modificar(dni, nombre, apellido):
         for i, cliente in enumerate(Clientes.lista):
             if cliente.dni == dni:
-                
+                Clientes.lista[i].nombre = nombre
+                Clientes.lista[i].apellido = apellido
+                Clientes.guardar()
+                return Clientes.lista[i]
+    
+    @staticmethod
+    def borrar(dni):
+        for i, cliente in enumerate(Clientes.lista):
+            if cliente.dni == dni:
+                cliente = Clientes.lista.pop(i)
+                Clientes.guardar()
+                return cliente
