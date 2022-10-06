799bd62eda9039c2006afa58db04acab467ec674
crltsnch <crltsnch@myuax.com>
2022-10-06 11:38:02 +0200

    Paso 3

--- Gestor de Clientes/gestor/tests/test_database.py
+++ Gestor de Clientes/gestor/tests/test_database.py
@@ -9,9 +9,11 @@ class TestDatabase(unittest.TestCase):
 
     def setUp(self):
         #se ejecuta antes de cada prueba
-        db.Cliente('15J', 'Marta', 'Pérez'),
-        db.Cliente('48H', 'Manolo', 'López'),
-        db.Cliente('28Z', 'Ana', 'García')
+        db.Clientes.lista = [
+            db.Cliente('15J', 'Marta', 'Pérez'),
+            db.Cliente('48H', 'Manolo', 'López'),
+            db.Cliente('28Z', 'Ana', 'García')
+        ]
     
     def test_buscar_cliente(self):
         cliente_existente = db.Clientes.buscar('15J')
@@ -42,3 +44,20 @@ class TestDatabase(unittest.TestCase):
         self.assertFalse(helpers.dni_valido('23223S', db.Clientes.lista))
         self.assertFalse(helpers.dni_valido('F35', db.Clientes.lista))
         self.assertFalse(helpers.dni_valido('48H', db.Clientes.lista))
+    
+    def test_escritura_csv(self):
+        db.Clientes.borrar('48H')
+        db.Clientes.borrar('15J')
+        db.Clientes.modificar('28Z', 'Mariana', 'Pérez')
+
+        dni, nombre, apellido=None, None, None
+        with open (config.DATABASE_PATH, newline="\n") as fichero:
+            reader = csv.reader(fichero, delimiter=";")
+            dni, nombre, apellido = next(reader) #primera linea del iterador
+        
+        self.assertEqual(dni, '28Z')
+        self.assertEqual(nombre, 'Mariana')
+        self.assertEqual(apellido, 'Pérez')
+
+if __name__ == '__main__':
+    unittest.main()
